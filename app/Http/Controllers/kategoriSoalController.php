0b8e02f29d90b3f0dd74f2fe0b9d8a9c7bc6d0c6
miqdad-aad <aadfahmi@gmail.com>
2024-01-31 20:32:45 +0700

    a

--- app/Http/Controllers/kategoriSoalController.php
+++ app/Http/Controllers/kategoriSoalController.php
@@ -11,8 +11,9 @@ class kategoriSoalController extends Controller
 {
     public function masterKategoriSoal(Request $request)
     {
+        $data = DB::table('m_kategori_soal')->get();
+        dd($data);
         if($request->ajax() ){
-           $data = kategorisoal;
             return DataTables::of($data)
                     ->addIndexColumn()
                     ->addColumn('action', function($row){
