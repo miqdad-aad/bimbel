20a2ef044d45684834dfb555dd63e483cce075e9
Yoki Hidayatur Rohman <yokihidayaturr13@gmail.com>
2024-01-31 20:31:58 +0700

    dsf

---
+++ app/Models/KategoriSoalModels.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KategoriSoalModels extends Model
+{
+    use HasFactory;
+    protected $table ="m_kategori_soal";
+    protected $guard = "*";
+}
