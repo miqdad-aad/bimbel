d1941551dad6371fb89184dbc9b5e91e21095222
Yoki Hidayatur Rohman <yokihidayaturr13@gmail.com>
2024-03-04 20:52:50 +0700

    sdf

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,6 @@ use App\Http\Controllers\MasterAkunController;
 |
 */
 
-
 Route::controller(SiswaController::class)->group(function () {
     Route::get('/exam', 'exam')->name('exam');
     Route::get('/soal_exam', 'soal_exam')->name('soal_exam');
